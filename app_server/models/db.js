838a70eba39346cc555b614f56df0221f15d1e5a
omerburakkaya <omerrburakkaya@gmail.com>
2022-11-11 17:46:27 +0300

    Dosyalar Yüklendi

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 require("./mekansema")
-var dbURI = 'mongodb://localhost/mekanbul';
+var dbURI = 'mongodb+srv://burak:1234@mekanbul.rjaedsg.mongodb.net/?retryWrites=true&w=majority';
 mongoose.connect(dbURI);
 
 function kapat(msg, callback)
